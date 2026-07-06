40e5250bf6b3d84531df9f6c5923f9c09c55a28a
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-06 19:43:53 +0300

    Export social neighbor utilities

--- src/safecrossai/social/__init__.py
+++ src/safecrossai/social/__init__.py
@@ -6,17 +6,22 @@ be shared across classical and learning-based predictors.
 """
 
 from .geometry import (
+    bearing,
     distance,
+    heading,
     relative_position,
     relative_velocity,
-    heading,
-    bearing,
 )
+from .neighbors import Neighbor, SocialAgent, find_neighbors, k_nearest_neighbors
 
 __all__ = [
+    "bearing",
     "distance",
+    "heading",
     "relative_position",
     "relative_velocity",
-    "heading",
-    "bearing",
+    "Neighbor",
+    "SocialAgent",
+    "find_neighbors",
+    "k_nearest_neighbors",
 ]
