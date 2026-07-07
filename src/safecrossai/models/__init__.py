046ca498c5f6b8a590423d1397f15efa2dd9574a
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-07 16:47:58 +0300

    Add model API package

---
+++ src/safecrossai/models/__init__.py
@@ -0,0 +1,6 @@
+"""Model interfaces and outputs for SafeCrossAI."""
+
+from safecrossai.models.base import TrajectoryPredictor
+from safecrossai.models.outputs import PredictionOutput
+
+__all__ = ["TrajectoryPredictor", "PredictionOutput"]
