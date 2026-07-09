bc31aa07edca789774684d0d1b12ade6b5d0adfc
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-09 22:22:21 +0300

    Add synthetic intersection simulation package

---
+++ src/safecrossai/simulation/__init__.py
@@ -0,0 +1,5 @@
+"""Synthetic intelligent-intersection simulation package."""
+
+from safecrossai.simulation.intersection_simulator import SyntheticIntersectionSimulator
+
+__all__ = ["SyntheticIntersectionSimulator"]
